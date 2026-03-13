78b14f50c3ba18a71dece0172488539d063aff2e
Nauno40 <batty.arnaud@hotmail.fr>
2026-03-13 18:42:26 +0100

    refactor: remove Type, NPrompt, and Extrainfo fields from the Artist type and corresponding JSON data.

--- src/types/artist.ts
+++ src/types/artist.ts
@@ -1,15 +1,12 @@
 export type ModelType = string;
 
 export interface Artist {
-    Type: string;
     Name: string;
     Born: string;
     Death: string;
     Prompt: string;
-    NPrompt: string;
     Category: string;
     Checkpoint: string;
-    Extrainfo: string;
     Image: string;
     Creation: string;
     Model: ModelType;
